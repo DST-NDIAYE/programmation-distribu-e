625b761d1e55e431647bfe9f6a2d89eb8cc77700
Serigne Bassirou Ndiaye <dstndiaye@gmail.com>
2025-01-17 10:18:30 +0100

    ajoue service GET

--- src/main/java/TP/amsd/MaClasse.java
+++ src/main/java/TP/amsd/MaClasse.java
@@ -1,12 +1,33 @@
 package TP.amsd;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MaClasse {
+
+    Logger logger = LoggerFactory.getLogger(MaClasse.class);
+
     @GetMapping("/")
-public String hello() {
-return "Bonjour tout le monde je suis Serigne Ndiaye";
-}
+    public String hello() {
+        return "Bonjour tout le monde je suis Serigne Ndiaye";
+    }
+
+    @PutMapping(value = "/cars/{plateNumber}")
+    public void rent(
+            @PathVariable("plateNumber") String plateNumber,
+            @RequestParam(value = "rent", required = true) boolean rent,
+            @RequestBody Dates dates) {
+
+        logger.info("PlateNumber:" + plateNumber);
+        logger.info("Rent:" + rent);
+        logger.info("Dates:" + dates);
+    }
+
 }
